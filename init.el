132f74a8564fdcd03e8f699d383b15d881fd0413
Opioid <benjamin.savs@gmail.com>
2016-07-19 19:36:43 +0200

    Switched back to linum

--- init.el
+++ init.el
@@ -62,12 +62,13 @@
 (add-to-list 'auto-mode-alist '("\\.take\\'" . json-mode))
 
 ;; Line numbering
-;; (global-linum-mode 1)
-;; (setq linum-format "%4d")
-;; The above is the built-in way (?) but much slower for large files
-(use-package nlinum)
-(global-nlinum-mode 1)
-(setq nlinum-format "%4d")
+(global-linum-mode 1)
+(setq linum-format "%4d")
+;; The above is the "built-in" way but much slower for large files
+;; The below however gives trouble with Inconsolata-g
+;; (use-package nlinum)
+;; (global-nlinum-mode 1)
+;; (setq nlinum-format "%4d")
 
 ;; (use-package icicles)
 ;; (icy-mode 1)
