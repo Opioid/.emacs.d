f592573644dea668df575a62e42f0aef6e24b3e5
Benjamin Savs <Benjamin.SAVS@3ds.com>
2016-08-03 13:58:07 +0200

    Enabled ido-vertical-mode

--- init.el
+++ init.el
@@ -90,6 +90,9 @@
 (use-package ido-ubiquitous)
 (ido-ubiquitous-mode t)
 
+(use-package ido-vertical-mode)
+(ido-vertical-mode t)
+
 (use-package smex)
 ;; Can be omitted. This might cause a (minimal) delay
 ;; when Smex is auto-initialized on its first run.
@@ -110,3 +113,17 @@
 
 ;; For some reason "delete-selection-mode" gets disabled again under Linux if placed near the top of the file
 (delete-selection-mode t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+	(ido-vertical-mode smex ido-ubiquitous flx-ido use-package nlinum monokai-theme json-mode icicles company auto-complete adoc-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
