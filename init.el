75f497e61b57fa121d92d35f582d66011a1558be
Benjamin Savs <Benjamin.SAVS@3ds.com>
2016-08-04 10:56:39 +0200

    Added highlight-thing

--- init.el
+++ init.el
@@ -111,19 +111,8 @@
 							(buffer-face-mode t)
 							(nlinum-mode 0)))
 
+(use-package highlight-thing)
+(global-highlight-thing-mode)
+
 ;; For some reason "delete-selection-mode" gets disabled again under Linux if placed near the top of the file
 (delete-selection-mode t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-	(ido-vertical-mode smex ido-ubiquitous flx-ido use-package nlinum monokai-theme json-mode icicles company auto-complete adoc-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
