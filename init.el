580be7181a02219793265c6dbaf4b3d8dbab00f8
Benjamin Savs <Benjamin.SAVS@3ds.com>
2016-09-19 13:42:17 +0200

    Maybe disabled horizontal scrollbar

--- init.el
+++ init.el
@@ -23,6 +23,7 @@
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
+(setq horizontal-scroll-bars nil)
 (setq-default tab-width 4)
 (setq sgml-basic-offset 4) ;; In my case basically tab-width for HTML
 (setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
