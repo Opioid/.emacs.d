94eb5f4b96f3bd2ef69f0a8b894f233b236cb986
Opioid <benjamin.savs@gmail.com>
2016-09-22 23:29:25 +0200

    Incread line spacing to 2

--- init.el
+++ init.el
@@ -17,6 +17,8 @@
   (set-default-font "Inconsolata_g 11")
   (setq buffer-face-mode-face '(:family "Noto" :height 120 :weight medium)))
 
+(setq-default line-spacing 2)
+
 (setq gc-cons-threshold 20000000) ;; Garbage collection to ca. 20 MB
 (desktop-save-mode 1)
 (global-auto-revert-mode t) ;; Update files changed another process
@@ -142,19 +144,3 @@
 
 ;; For some reason "delete-selection-mode" gets disabled again under Linux if placed near the top of the file
 (delete-selection-mode t)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(delete-selection-mode nil)
- '(package-selected-packages
-   (quote
-	(markdown-mode use-package smex nlinum monokai-theme json-mode ido-vertical-mode ido-ubiquitous highlight-thing flx-ido company auto-complete adoc-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
