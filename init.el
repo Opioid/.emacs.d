39ea7016a8ee16a17af45329805c23cc22d05987
Benjamin Savs <Benjamin.SAVS@3ds.com>
2017-06-28 14:28:47 +0200

    Maintenance

--- init.el
+++ init.el
@@ -222,17 +222,3 @@
 
 ;; For some reason "delete-selection-mode" gets disabled again under Linux if placed near the top of the file
 (delete-selection-mode t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-	(use-package smex nlinum monokai-theme markdown-mode json-mode ido-vertical-mode ido-ubiquitous highlight-symbol flx-ido company anzu ag))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
