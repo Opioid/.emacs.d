b4de9c02179c3da3c822465757ab9dab498fb383
Benjamin Savs <Benjamin.SAVS@3ds.com>
2017-07-19 14:30:30 +0200

    Dired lists directories first now

--- init.el
+++ init.el
@@ -147,6 +147,27 @@
 ;; (use-package iedit)
 
 ;;==============================================================================
+;; dired-mode
+;;==============================================================================
+
+;; List directories before files
+
+(defun mydired-sort ()
+  "Sort dired listings with directories first."
+  (save-excursion
+    (let (buffer-read-only)
+      (forward-line 2) ;; beyond dir. header 
+      (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max)))
+    (set-buffer-modified-p nil)))
+
+(defadvice dired-readin
+  (after dired-after-updating-hook first () activate)
+  "Sort dired listings with directories first before adding marks."
+  (mydired-sort))
+
+;;==============================================================================
+
+;;==============================================================================
 ;; Defaults for occur and multi-occur
 ;;==============================================================================
 
