10d411e6976438273d69fbfe93ecc6dc562de65b
Benjamin Savs <Benjamin.SAVS@3ds.com>
2017-07-31 10:42:59 +0200

    Added eyebrowse mode

--- init.el
+++ init.el
@@ -236,6 +236,9 @@
 (global-set-key [f3] 'highlight-symbol-next)
 (global-set-key [(meta f3)] 'highlight-symbol-query-replace)
 
+(use-package eyebrowse)
+(eyebrowse-mode t)
+
 ;;==============================================================================
 ;; etags
 ;;==============================================================================
