bf9e061ee95676ce5988fb782df5d9df551e7a16
Opioid <benjamin.savs@gmail.com>
2018-11-06 15:23:54 +0100

    Deleting custom-set-variables that should never have been checked in

--- init.el
+++ init.el
@@ -673,17 +673,3 @@
 ;;==============================================================================
 ;; Stuff appended by emacs that we don't really want
 ;;==============================================================================
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (yaml-mode web-mode use-package shader-mode rainbow-mode rainbow-delimiters nlinum-hl multiple-cursors moody monokai-theme markdown-mode magit json-mode js2-mode iedit glsl-mode flyspell-correct-ivy flx expand-region diminish counsel-projectile counsel-etags company cmake-mode adoc-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
